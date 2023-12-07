6116c83ed179eb98bd93e5cfdf0fbe5e68de88e0
Stefano-Labianca <lstefano@protonmail.com>
2023-12-07 17:39:54 +0100

    Implemented DFS for CSP resolution

--- csp_problem/algorithm/dfs.py
+++ csp_problem/algorithm/dfs.py
@@ -1,9 +1,26 @@
+from csp_problem.Constraint import Constraint
 from csp_problem.resolver import Resolver
+from csp_problem.Variable import Variable
 
 
 class DFS(Resolver):
-    
-    def solve(self) -> list:
-        valid_appliances = []
 
-        return []
+    def solve(self, variables: list[Variable], constraints: list[Constraint]):
+        return list(self.__dfs(variables, constraints))
+
+    def __dfs(self, variables: list[Variable], constraints: list[Constraint], context={}):
+        eval_cons = {c for c in constraints if c.can_evaluate(context)}
+
+        if all(c.evaluate(context) for c in eval_cons):
+            if len(variables) == 0:
+                yield context
+            else:
+                remaining_cons = [c for c in constraints if c not in eval_cons]
+                variable = variables[0]
+
+                for value in variable.domain:
+                    yield from self.__dfs(
+                        variables[1:],
+                        remaining_cons,
+                        context | {variable.name: value}
+                    )
