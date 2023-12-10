3a68fd9c5bcc3f5964af194220e2dc8f3d58f44f
Stefano-Labianca <lstefano@protonmail.com>
2023-12-10 12:44:07 +0100

    Testing GAC Algorithm

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# from time import process_time_ns
+from time import time
 
 from rich.console import Console
-from rich.table import Table
 
 from appliance.appliances_controller import (create_appliances,
                                              create_variables,
                                              get_variables_name)
 from csp_problem.algorithm.dfs import DFS
+from csp_problem.algorithm.gac import GAC
 from csp_problem.Constraint import Constraint
 from csp_problem.csp import CSP
 from csp_problem.Variable import Variable
 
+# from rich.table import Table
+
 
 def limit_multimedia(assignment: dict[Variable, int]) -> bool:
-
     accumulator = 0
 
     for v in assignment:
         accumulator = accumulator + (assignment[v] * 24)
-    return accumulator < 130_000
+    return accumulator < 35_000
 
 
 def limit_cooling(assignment: dict[Variable, int]) -> bool:
-
     accumulator = 0
 
     for v in assignment:
         accumulator = accumulator + (assignment[v] * 24)
-    return accumulator < 100_000
+    return accumulator < 15_000
 
 
-table = Table(title="Info", highlight=True)
+# table = Table(title="Info", highlight=True)
 console = Console()
 
 
@@ -38,15 +38,15 @@ appliances = create_appliances()
 variables = create_variables(appliances)
 variables_name = get_variables_name(variables)
 
-columns = [
-    "computer", "3D_printer", "internet_router", "laptop",
-    "phone_charger", "printer", "monitor", "tv", "sound_system",
-    "air_conditioner", "fan", "air_purifier"
-]
+# columns = [
+#     "computer", "3D_printer", "internet_router", "laptop",
+#     "phone_charger", "printer", "monitor", "tv", "sound_system",
+#     "air_conditioner", "fan", "air_purifier"
+# ]
 
 
-for c in columns:
-    table.add_column(c)
+# for c in columns:
+#     table.add_column(c)
 
 constraints = [
     Constraint(limit_multimedia, [
@@ -85,18 +85,33 @@ constraints = [
     Constraint(b_lt_c, ['B', 'C']),
 ]"""
 
+
 csp_problem = CSP(variables, constraints)
 dfs = DFS()
+gac = GAC(variables, constraints)
 
-solutions = csp_problem.solve(dfs)
-restricted_values = {}
-
-for s in solutions:
-    row = dict([
-        (k, str(v)) for k, v in s.items() if k in columns
-    ])
-
-    table.add_row(*row.values(), style="cyan")
+variables = gac.make_arc_consistent()
+console.log(variables)
 
 
-console.log(table)
+# TODO: Creare la classe che permette di risolvere il CSP tramite GAC
+# TODO: Continuare l'implementazione del GAC
+# TODO: Fare i test con il dfs
+
+
+# Uso del DFS
+# start = time()
+# solutions = csp_problem.solve(dfs)
+# end = time()
+
+# print("Time: ", end - start, "s")
+
+# for s in solutions:
+#     row = dict([
+#         (k, str(v)) for k, v in s.items() if k in columns
+#     ])
+
+#     table.add_row(*row.values(), style="cyan")
+
+
+# console.log(table)
