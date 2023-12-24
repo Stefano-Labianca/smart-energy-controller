601534522dbc85d42d5ed062df8106e6c60ef6c3
Stefano-Labianca <lstefano@protonmail.com>
2023-12-24 12:25:14 +0100

    First integration test

---
+++ main.py
@@ -0,0 +1,72 @@
+from time import time
+
+from rich.console import Console
+
+from appliance.appliances_controller import (create_appliances,
+                                             create_variables,
+                                             get_variables_name)
+from csp_problem.algorithm.dfs import DFS
+from csp_problem.algorithm.gac import GAC
+from csp_problem.Constraint import Constraint
+from csp_problem.csp import CSP
+from ontology.ontology_controller import ApplianceOntology
+from utils.printer import assignments_printer, partial_assignments_printer
+
+
+def limit_multimedia(assignment: dict[str, int]) -> bool:
+    accumulator = 0
+
+    for v in assignment:
+        accumulator = accumulator + (assignment[v] * 24)
+    return accumulator < 35_000
+
+
+def limit_cooling(assignment: dict[str, int]) -> bool:
+    accumulator = 0
+
+    for v in assignment:
+        accumulator = accumulator + (assignment[v] * 24)
+    return accumulator < 15_000
+
+
+console = Console()
+
+onto = ApplianceOntology()
+appliances = onto.get_all_individuals()
+
+variables = create_variables(appliances)
+variables_name = get_variables_name(variables)
+
+
+constraints = [
+    Constraint(limit_multimedia, [
+        "computer", "3D_printer", "internet_router", "laptop",
+        "phone_charger", "printer", "monitor", "tv", "sound_system"
+    ]),
+    Constraint(limit_cooling, [
+        "air_conditioner", "fan", "air_purifier"
+    ]),
+]
+
+
+names = [
+    "computer", "3D_printer", "internet_router", "laptop",
+    "phone_charger", "printer", "monitor", "tv", "sound_system",
+    "air_conditioner", "fan", "air_purifier"
+]
+
+csp_problem = CSP(variables, constraints)
+
+dfs = DFS(csp_problem)
+gac = GAC(csp_problem)
+
+solutions = []
+
+start = time()
+solutions = dfs.solve()
+# solutions = gac.solve()
+end = time()
+
+partial_assignments_printer(solutions, names)
+# assignments_printer(solutions)
+console.print(f"Time: {round((end - start) * 1000, 3)}ms", style="i")
