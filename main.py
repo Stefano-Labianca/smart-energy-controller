f3140e772ffb7195b47eeb8bce299f8d3309a27c
Stefano-Labianca <lstefano@protonmail.com>
2023-12-26 11:24:40 +0100

    Test for query helper

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from ontology.ontology_controller import ApplianceOntology
+from ontology.sparql import SPARQL, OWLType
 
 ontology = ApplianceOntology()
+query_manager = SPARQL()
+
+query_manager.select(['x']).where(
+    [OWLType.NAMED_INDIVIDUAL]
+).execute()
+
 # print(ontology.get_all_classes())
 # appliances = ontology.get_all_individuals()
 
@@ -14,12 +21,12 @@ ontology = ApplianceOntology()
 # )
 
 
-print(
-    ontology.search_all("Appliance"),
-    ontology.search_all("Cooling"),
-    ontology.search_all("Kitchen"),
-    ontology.search_all("Washing"),
-    ontology.search_all("Multimedia"),
-    ontology.search_all("Other"),
-    sep="\n\n"
-)
+# print(
+#     ontology.search_all("Appliance"),
+#     ontology.search_all("Cooling"),
+#     ontology.search_all("Kitchen"),
+#     ontology.search_all("Washing"),
+#     ontology.search_all("Multimedia"),
+#     ontology.search_all("Other"),
+#     sep="\n\n"
+# )
