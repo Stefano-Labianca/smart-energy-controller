f9c4a8fe8a60d03974c1b400025a4d14213b4215
Stefano-Labianca <lstefano@protonmail.com>
2024-01-11 09:48:28 +0100

    Added input validation for adding new appliances

--- cli/ontology_cli.py
+++ cli/ontology_cli.py
@@ -16,11 +16,11 @@ INVALID_CONTENT = '[^a-zA-Z0-9_-]+'
 
 
 class OntologyCLI:
-    """Permette all'utente di interagire con l'ontologia
+    """Permette all'utente di interagire con l'ontologia.
     """
 
-    """Serve per indicare all'utente quanti nuovi elementi vuole salvare
-    all'interno dell'ontologia
+    """Serve per indicare all'utente quanti, nuovi elementi, vuole salvare
+    all'interno dell'ontologia.
     """
     loaded_info: int = 0
 
@@ -117,8 +117,11 @@ class OntologyCLI:
             Appliance: Dispositivo elettronico dell'utente.
         """
 
-        # TODO: Validazione input
         appliance = Appliance()
+        name = ""
+        energy = []
+        category = ""
+        size = ""
 
         # Perché un'istanza della classe ha questa lista di consumi?
         #   Nel caso si abbiano più dispositivi uguali, tipo 2 computer,
@@ -128,30 +131,62 @@ class OntologyCLI:
         # TODO: Va messo nella docs
 
         while True:
-            console.print("Inserire il nome del dispositivo elettronico: ")
+            console.print(
+                "Inserire il nome del dispositivo elettronico: ",
+                style="blue"
+            )
             name = input().strip(" ")
 
-            console.print(
-                "\nInserire il consumo energetico.\nSe hai piu' dispositivi dello stesso tipo, separa i loro consumi con la virgola: "
-            )
-            energy = list(
-                map(
-                    lambda e: int(e),
-                    input().strip(" ").split(", ")
-                )
-            )
+            if cls.__check_name(name):
+                break
 
+            console.print("Nome non valido.", style="red")
+
+        while True:
             console.print(
-                "\nInserire la categoria (Kitchen, Multimedia, Cooling, Washing, Other): "
+                "\nInserire il consumo energetico.\nSe hai piu' dispositivi dello stesso tipo, separa i loro consumi con uno spazio: ",
+                style="blue"
+            )
+            energy_input = input().strip(" ")
+            energy_input = re.sub(r'\s+', " ", energy_input)
+
+            if cls.__check_energy_consumption(energy_input):
+                energy = list(
+                    map(
+                        lambda e: int(e),
+                        energy_input.split(" ")
+                    )
+                )
+
+                break
+
+            console.print("Valori non validi.", style="red")
+
+        while True:
+            console.print(
+                "\nInserire la categoria (Kitchen, Multimedia, Cooling, Washing, Other): ",
+                style="blue"
             )
             category = input().strip(" ")
 
+            if cls.__check_category(category):
+                category = category.lower().capitalize()
+                break
+
+            console.print("Categoria non valida.", style="red")
+
+        while True:
             console.print(
-                "\nInserire le dimensioni (Small, Medium, Large): "
+                "\nInserire le dimensioni (small, medium, large): ",
+                style="blue"
             )
             size = input().strip(" ")
 
-            break
+            if cls.__check_size(size):
+                size = size.lower()
+                break
+
+            console.print("Dimensioni non valide.", style="red")
 
         appliance = appliance.name(name).category(
             category).energy_consumption(energy).size(size)
@@ -184,3 +219,54 @@ class OntologyCLI:
             return False
 
         return True
+
+    @classmethod
+    def __check_name(cls, user_input: str) -> bool:
+        if len(user_input) == 0:
+            return False
+
+        if not bool(re.search(VALID_FIRST_CHAR, user_input)):
+            return False
+
+        if bool(re.search(INVALID_CONTENT, user_input)):
+            return False
+
+        return True
+
+    @classmethod
+    def __check_energy_consumption(cls, user_input: str) -> bool:
+        if len(user_input) == 0:
+            return False
+
+        if not bool(re.search(r'^(\d+\s)*\d+$', user_input)):
+            return False
+
+        return True
+
+    @classmethod
+    def __check_category(cls, user_input: str) -> bool:
+        if len(user_input) == 0:
+            return False
+
+        user_input = user_input.lower().capitalize()
+
+        match user_input:
+            case 'Multimedia': return True
+            case 'Kitchen': return True
+            case 'Washing': return True
+            case 'Cooling': return True
+            case 'Other': return True
+            case _: return False
+
+    @classmethod
+    def __check_size(cls, user_input: str) -> bool:
+        if len(user_input) == 0:
+            return False
+
+        user_input = user_input.lower()
+
+        match user_input:
+            case 'small': return True
+            case 'medium': return True
+            case 'large': return True
+            case _: return False
